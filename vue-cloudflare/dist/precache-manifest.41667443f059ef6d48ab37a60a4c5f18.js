650da2c175119638a9424ccc0032b1e46a12860c
Ricky Bosch <rbosch@squiz.co.nz>
2021-05-26 13:16:07 +1200

    added dist

---
+++ vue-cloudflare/dist/precache-manifest.41667443f059ef6d48ab37a60a4c5f18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5463625577ee2fdc5fec",
+    "url": "/js/about.036d1df6.js"
+  },
+  {
+    "revision": "dd80df93e5e181867103",
+    "url": "/css/app.ba629074.css"
+  },
+  {
+    "revision": "dd80df93e5e181867103",
+    "url": "/js/app.527f0bf2.js"
+  },
+  {
+    "revision": "b3b5e45a6da33919e50b",
+    "url": "/js/chunk-vendors.054add8e.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "12e5c7ed74ccca23b8d52fe81dfb14b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
